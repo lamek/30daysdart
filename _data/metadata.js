6c2b19e4a3794d5e115b8d6e2ad4df737a971bbb
lamek <kevin.lamenzo@gmail.com>
2025-03-25 08:52:38 -0700

    more metadata updates

--- _data/metadata.js
+++ _data/metadata.js
@@ -2,7 +2,7 @@ export default {
 	title: "30 Days of Dart",
 	url: "https://lamek.github.io/30daysdart/",
 	language: "en",
-	description: "Copy of Leetcodes 30 Days of JS, but for Dart",
+	description: "Copy of Leetcode's 30 Days of JS, but for Dart",
 	author: {
 		name: "Kevin Lamenzo",
 		email: "kevin.lamenzo@gmail.com",
